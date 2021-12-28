cb54c7ad71ae94004093b82f92aecad85d1c0e71
belajarwebdevelopment <88145570+belajarwebdevelopment@users.noreply.github.com>
2021-12-28 15:52:52 +0700

    Add Files via Upload
    bkd.html dan bkd.js

---
+++ bkd.js
@@ -0,0 +1,88 @@
+
+function tabelBKD(max,e) {
+    const is_eInRange = (min, max, val) => {
+        if (val <= max && val >= min) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const is_nInRange = (min, max, val) => {
+        if (val <= max && val >= min) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+
+    return {
+        //max : max,
+        //e : e,
+        getClass : () => {
+            let n = max/e;
+            const Kelas = {
+                I : [is_eInRange(0.001,Infinity,e) && is_nInRange(50000,Infinity,n), 100*e, {
+                    range1 : {mn : 0*e, mx : 50000*e, bkdTera : 0.5*e}, 
+                    range2 : {mn : 50000*e, mx : 200000*e, bkdTera : 1*e}, 
+                    range3 : {mn : 200000*e, mx : Infinity*e, bkdTera : 1.5*e}
+                }],
+                II : [is_eInRange(0.001,0.05,e) && is_nInRange(100,100000,n), 20*e, {
+                    range1 : {mn : 0*e, mx : 5000*e, bkdTera : 0.5*e}, 
+                    range2 : {mn : 5000*e, mx : 20000*e, bkdTera : 1*e}, 
+                    range3 : {mn : 20000*e, mx : 100000*e, bkdTera : 1.5*e}
+                }],
+                II_ : [is_eInRange(0.1,Infinity,e) && is_nInRange(5000,100000,n), 50*e, {
+                    range1 : {mn : 0*e, mx : 5000*e, bkdTera : 0.5*e}, 
+                    range2 : {mn : 5000*e, mx : 20000*e, bkdTera : 1*e}, 
+                    range3 : {mn : 20000*e, mx : 100000*e, bkdTera : 1.5*e}
+                }],
+                III : [is_eInRange(0.1,2,e) && is_nInRange(100,10000,n), 20*e, {
+                    range1 : {mn : 0*e, mx : 500*e, bkdTera : 0.5*e}, 
+                    range2 : {mn : 500*e, mx : 2000*e, bkdTera : 1*e}, 
+                    range3 : {mn : 2000*e, mx : 10000*e, bkdTera : 1.5*e}
+                }],
+                III_ : [is_eInRange(5,Infinity,e) && is_nInRange(500,10000,n), 20*e, {
+                    range1 : {mn : 0*e, mx : 500*e, bkdTera : 0.5*e}, 
+                    range2 : {mn : 500*e, mx : 2000*e, bkdTera : 1*e}, 
+                    range3 : {mn : 2000*e, mx : 10000*e, bkdTera : 1.5*e}
+                }],
+                IIII : [is_eInRange(5,Infinity,e) && is_nInRange(100,1000,n), 10*e, {
+                    range1 : {mn : 0*e, mx : 50*e, bkdTera : 0.5*e}, 
+                    range2 : {mn : 50*e, mx : 200*e, bkdTera : 1*e}, 
+                    range3 : {mn : 200*e, mx : 1000*e, bkdTera : 1.5*e}
+                }],
+            }
+
+            return {max, e, n, Kelas};            
+        },
+    };
+
+}
+
+
+const uKelas = tabelBKD(30000,5).getClass()['Kelas'];
+const uKelasFiltered = Object.keys(uKelas)
+    .filter(key => uKelas[key][0] == true)
+    .reduce((preVal,curVal) => (preVal[curVal] = uKelas[curVal], preVal),{});
+
+const tag = Object.keys(uKelasFiltered).forEach((a,index) => {
+    //console.log(Object.values(uKelasFiltered[a][2]));
+    let arr = Object.values(uKelasFiltered[a][2]);
+    let konten = `${arr.map(idx => `<tr><td>
+        ${idx['mn']} - ${idx['mx']} g
+    </td><td>
+        ${idx['bkdTera']} g
+    </td><td>
+        ${idx['bkdTera']*2} g
+    </td>`).join('')}<td rowspan='2'>${uKelasFiltered[a][1]} g</td></tr>`;
+
+    document.querySelector(`#dataJudul${index+1}`).hidden = false;
+    document.querySelector(`#dataJudul${index+1}`).innerHTML = `Kelas ${a}`;
+    document.querySelector(`#dataTabel${index+1}`).hidden = false;
+    document.querySelector(`#konten${index+1}`).innerHTML = konten;
+});
+
+// console.dir(uKelasFiltered);
+// console.dir(tag);
+
