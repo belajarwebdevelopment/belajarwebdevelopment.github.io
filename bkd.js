5b5afca35fbee203f385884b4152ba9a214b9e89
belajarwebdevelopment <88145570+belajarwebdevelopment@users.noreply.github.com>
2022-02-16 10:38:15 +0700

    Change code to clear e and d input when kap max changed v3
    Change code to clear e and d input when kap max changed v3

--- bkd.js
+++ bkd.js
@@ -197,6 +197,6 @@ loadInput.addEventListener("keyup", () => {
 
 let maxInputChange = document.getElementById("max");
 maxInputChange.addEventListener("keyup", () => {
-    document.getEelementById("d").setAttribute('value','');
-    document.getEelementById("e").setAttribute('value','');    
+    document.getElementById("d").setAttribute('value','');
+    document.getElementById("e").setAttribute('value','');    
 });
