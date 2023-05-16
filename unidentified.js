f94267e1e3e40500d71f560f8e2d0e459f4b4b22
belajarwebdevelopment <belajar.webdevelopment@gmail.com>
2023-05-16 21:35:23 +0700

    manmabahkan uttp yg tdk terdeteksi stikernya 5

---
+++ unidentified.js
@@ -0,0 +1,65 @@
+async function getUttp() {
+    
+    const url = "https://script.google.com/macros/s/AKfycbz_Je2ZXuIRFmxWdYpQTbgWbEa4iqU9ucJj1FrPK8hoql-UtKnFbBj-upEFOvWU7ZYH/exec";
+    
+    await fetch(url)
+    .then(data => data.json())
+    .then(data => {
+        let temporary = [];
+        if (data.result === "success") {
+            data.data.forEach(e => {
+                temporary.push(e[0]);
+            });
+
+
+            filtered = [...new Set(temporary)];
+ 
+            let str = '';
+            filtered.forEach(e => {
+                str += `<option value='${e}'>${e}</option>`;
+                
+            });
+
+            document.getElementById("uttpList").innerHTML = str;
+
+        }
+        
+    });
+
+}
+
+async function getPasar() {
+    
+    const url = "https://script.google.com/macros/s/AKfycbwzVP84YKO62g10ShP-DKAqmOieh8VMJv_8L1FG6ZldOUNPnNFTTZgEKid6d8B6Dx6n/exec";
+    
+    await fetch(url)
+    .then(data => data.json())
+    .then(data => {
+        //console.log(data);
+        let temporary = [];
+        if (data.result === "success") {
+            data.data.forEach(e => {
+                temporary.push(e[0]);
+            });
+
+
+            filtered = [...new Set(temporary)];
+ 
+            let str = '';
+            filtered.forEach(e => {
+                str += `<option value='${e}'>${e}</option>`;
+                
+            });
+
+            document.getElementById("pasarList").innerHTML = str;
+
+        }
+        
+    });
+
+}
+
+(function main() {
+    getUttp();
+    getPasar();
+})();
